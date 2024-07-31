07695f62c030a5cd79dd278d996a6a074cfaa0bb
Nathan G. Wiseman <55075327+NGWi@users.noreply.github.com>
2024-07-31 13:05:48 -0400

    Solved Ruby Advanced Classes 01

--- practice-ruby/03-Advanced/05-custom-methods-classes4/01.rb
+++ practice-ruby/03-Advanced/05-custom-methods-classes4/01.rb
@@ -1,1 +1,33 @@
 # Write a ShoppingCart class that stores an array of items with methods to add an item, remove an item, and display all the items.
+
+class ShoppingCart
+  attr_accessor :items_array
+  
+  def initialize
+    @items_array = []
+  end
+  def add(item)
+    pp items_array
+    items_array << item
+  end
+
+  def remove(value)
+    if items_array.delete(value)
+    else items_array.delete_at(value)
+    end
+  end
+  alias_method :delete, :remove
+  def display
+    pp items_array
+  end
+end
+
+shopping_cart = ShoppingCart.new
+shopping_cart.add("Apples")
+shopping_cart.display
+shopping_cart.add("Pears")
+shopping_cart.display
+shopping_cart.delete("Apples")
+shopping_cart.display
+shopping_cart.remove(0)
+shopping_cart.display
