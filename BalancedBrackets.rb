0e0d35d889d6dbb43a7cfdd04fc699b89f63c353
Nathan G. Wiseman <55075327+NGWi@users.noreply.github.com>
2024-08-01 17:56:01 -0400

    Solved crazy example

---
+++ BalancedBrackets.rb
@@ -0,0 +1,75 @@
+example_a = "(x+3)[y{3}(z]"
+example_b = "([[{}]])"
+example_c = ""
+example_d = "(][)"
+new_example = "([{<>)}]"
+
+def balance(string)
+  puts string
+  closing = {"]"=>0,")"=>1,"}"=>2, ">"=>3}
+  opening = ["[","(","{","<"]
+  looking_for = nil
+  found_i = nil
+  index = 0
+  length = string.length
+  while index < length
+    while index < length
+      char = string[index]
+      if closing[char]
+        found_i = index
+        opening_i = closing[char]
+        looking_for = opening[opening_i]
+        wrong_brackets = opening.dup
+        wrong_brackets.delete_at(opening_i)
+        break
+      else
+        # puts "Not yet"
+      end
+      index += 1
+    end
+    
+    if looking_for
+      puts "Closing Brackets found at index: " + index.to_s
+      puts looking_for
+      puts "Wrong Brackets: " + wrong_brackets.to_s
+    else
+      puts "No Brackets"
+      puts "BALANCED"
+      return
+    end
+
+    index -= 1
+
+    found_match = false
+    while index >= 0
+      char = string[index]
+      if char == looking_for
+        puts "Found"
+        found_match = true
+        string.slice!(found_i)
+        string.slice!(index)
+        index = found_i - 1
+        break
+      elsif wrong_brackets.include?(char)
+        puts "NOT BALANCED"
+        return
+      end
+      index -= 1
+    end
+    if found_match == false
+      puts "NOT BALANCED"
+      return
+    end
+  end
+  puts "BALANCED"
+end
+
+balance(example_a)
+puts "-" * 20
+balance(example_b) 
+puts "-" * 20
+balance(example_c)
+puts "-" * 20
+balance(example_d)
+puts "-" * 20
+balance(new_example) 
