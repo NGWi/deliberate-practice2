550e52ab3ae4dfefe9ccac044179d8818faa77e2
Nathan G. Wiseman <55075327+NGWi@users.noreply.github.com>
2024-08-04 13:21:24 -0400

    Solved 04

--- practice-ruby/03-Advanced/05-custom-methods-classes4/04.rb
+++ practice-ruby/03-Advanced/05-custom-methods-classes4/04.rb
@@ -1,1 +1,11 @@
 # Write a Contact class that stores the name, age, and contact_info, where contact_info is a hash that stores any additional information about the contact.
+class Contact
+  attr_writer :name, :age, :contact_info
+end
+
+contact = Contact.new
+
+contact.name = "Joe"
+contact.age = 11
+contact.contact_info = {address: "123 ABC Place", phone_number: "18001112222"}
+pp contact
