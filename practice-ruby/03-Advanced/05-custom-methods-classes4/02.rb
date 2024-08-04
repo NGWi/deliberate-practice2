7e90e92b3debc39ed2e30cd525ab85e60d580644
Nathan G. Wiseman <55075327+NGWi@users.noreply.github.com>
2024-08-04 12:09:24 -0400

    Solved ruby adv class 03

--- practice-ruby/03-Advanced/05-custom-methods-classes4/02.rb
+++ practice-ruby/03-Advanced/05-custom-methods-classes4/02.rb
@@ -1,1 +1,10 @@
 # Write a Product class that stores the name, price, and metadata, where metadata is a hash that stores additional information about the product.
+class Product
+  attr_writer :name, :price, :metadata
+end
+
+product = Product.new
+product.name = "New Product"
+product.price = 5.99
+product.metadata = { Description: "Bla bla bla...", first_posted: "08_04_2024" }
+pp productA
