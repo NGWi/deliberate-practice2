4b14597d4e2ada1b710737e0f0b108a74021b913
Nathan G. Wiseman <55075327+NGWi@users.noreply.github.com>
2024-08-15 12:22:46 -0400

    Did 02.js

--- practice-javascript/01-Basic/01-variables1/02.js
+++ practice-javascript/01-Basic/01-variables1/02.js
@@ -1,2 +1,4 @@
-# Write a program that uses a variable to store a word, then prints that word with all capital letters.
+// Write a program that uses a variable to store a word, then prints that word with all capital letters.
 
+var a = "Word";
+console.log(a.toUpperCase());
