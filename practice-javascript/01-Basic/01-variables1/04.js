71772144d81fd3e6a772edbcb80350ac0f892eeb
Nathan G. Wiseman <55075327+NGWi@users.noreply.github.com>
2024-08-20 20:52:34 -0400

    Showed step-by-step of 4th js solution.

--- practice-javascript/01-Basic/01-variables1/04.js
+++ practice-javascript/01-Basic/01-variables1/04.js
@@ -1,2 +1,8 @@
 // Write a program that uses a variable to store a word, then prints that word in reverse order.
 
+const a = "Word";
+
+console.log(a);
+console.log(a.split(""));
+console.log(a.split("").reverse());
+console.log(a.split("").reverse().join(""));
