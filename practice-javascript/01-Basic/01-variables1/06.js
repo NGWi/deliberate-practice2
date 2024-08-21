5662f6ad2e2b175b95fbc9ee46b65f469570b856
Nathan G. Wiseman <55075327+NGWi@users.noreply.github.com>
2024-08-20 20:56:06 -0400

    Did 6th js problem.

--- practice-javascript/01-Basic/01-variables1/06.js
+++ practice-javascript/01-Basic/01-variables1/06.js
@@ -1,2 +1,5 @@
 // Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
 
+const a = "Two";
+const b = "words";
+console.log(a + " " + b);
