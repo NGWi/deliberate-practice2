93ab4cab29c6f81afdcd9376a7a4893a36d54712
Nathan G. Wiseman <55075327+NGWi@users.noreply.github.com>
2024-09-10 12:32:32 -0400

    Did some code golf.

---
+++ giraffe.rb
@@ -0,0 +1,17 @@
+h=" /)ii/)
+(o '')"
+m="
+   | |"
+e="___|
+   | ___ |
+   ||   ||"
+puts h+(m*gets.to_i)+e
+# one less char than lula:
+n=gets.to_i
+print" /)ii/)
+(o '')"
+print "\n   | |"*n
+puts"___|
+   | ___ |
+   ||   ||"
+   # could combine ideas
