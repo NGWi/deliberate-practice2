42e445dc19d3aad115cf9c49ae3cd45496262b81
Nathan G. Wiseman <55075327+NGWi@users.noreply.github.com>
2024-09-11 11:15:35 -0400

    One more point I left out.

--- giraffe.rb
+++ giraffe.rb
@@ -5,7 +5,7 @@ m="
 e="___|
    | ___ |
    ||   ||"
-puts h+(m*gets.to_i)+e # could have taken out parentheses
+puts h+(m*gets.to_i)+e # could have taken out parentheses, and embedded h and e here
 # one less char than lula:
 n=gets.to_i
 print" /)ii/)
