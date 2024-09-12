2452ea5c8ff115c1017cbe5f99b03a7885039c60
Nathan G. Wiseman <55075327+NGWi@users.noreply.github.com>
2024-09-12 14:00:29 -0400

    Added qs.

--- Blind 75/countConnectedComponents.py
+++ Blind 75/countConnectedComponents.py
@@ -1,3 +1,32 @@
+# # Count Connected Components
+# Solved 
+# There is an undirected graph with n nodes. There is also an edges array, where edges[i] = [a, b] means that there is an edge between node a and node b in the graph.
+
+# The nodes are numbered from 0 to n - 1.
+
+# Return the total number of connected components in that graph.
+
+# Example 1:
+
+# Input:
+# n=3
+# edges=[[0,1], [0,2]]
+
+# Output:
+# 1
+# Example 2:
+
+# Input:
+# n=6
+# edges=[[0,1], [1,2], [2,3], [4,5]]
+
+# Output:
+# 2
+# Constraints:
+
+# 1 <= n <= 100
+# 0 <= edges.length <= n * (n - 1) / 2
+
 class Solution:
     def mergeComponents(self, node: int, otherNode: int, connectedNodes: hash) -> bool:
         """Checks the connected nodes of each node to see if the two nodes are part of the same component. If they are not, it merges their connected nodes and returns True."""
