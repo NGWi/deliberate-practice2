d839dbffb2e031b73722fd10072eed55e9643f26
Nathan G. Wiseman <55075327+NGWi@users.noreply.github.com>
2024-09-12 17:46:11 -0400

    Thought I solved it...

--- Blind 75/foreignDictionary.py
+++ Blind 75/foreignDictionary.py
@@ -34,3 +34,71 @@
 # The input words will contain characters only from lowercase 'a' to 'z'.
 # 1 <= words.length <= 100
 # 1 <= words[i].length <= 100
+
+
+class Solution:
+    def addLetterOrder(self, letter, other, lowerLetterHash, higherLetterHash):
+        lowerLetterHash[letter] = (lowerLetterHash.get(letter, set()) |
+                                   {other} 
+                                #    | lowerLetterHash.get(other, set())
+                                )
+        higherLetterHash[other] = (higherLetterHash.get(other, set()) |
+                                   {letter} 
+                                #    | higherLetterHash.get(letter, set())
+                                )
+
+    def compareLetters(self, words: List[str], lowerLetterHash, higherLetterHash):
+        index = 0
+        while index < len(words) - 1:
+            word_a = words[index]
+            word_b = words[index + 1]
+            # print(word_a, word_b)
+            l_index = 0
+            while l_index < len(word_a) and l_index < len(word_b):
+                letter = word_a[l_index]
+                other = word_b[l_index]
+                # print(letter, other)
+                if letter != other:
+                    # print(letter, other)
+                    self.addLetterOrder(letter, other, lowerLetterHash, higherLetterHash)
+                    # print(lowerLetterHash, higherLetterHash)
+                    break
+                l_index += 1
+            index += 1
+    def mergeSets(self, letterHash):
+        for letter in list(letterHash.keys()):
+                mergedSet = set()
+                to_process = [letter]
+                while to_process:
+                    current = to_process.pop()
+                    if current not in letterHash:
+                        letterHash[current] = set()
+                    for other in letterHash.get(current, set()): # will automatically have an empty set because of the previous line, so I canjust do .get(current)
+                        if other not in mergedSet:
+                            mergedSet.add(other)
+                            to_process.append(other)
+                letterHash[letter] = mergedSet
+    def validateHash(self, letterHash) -> bool:
+        for letter in list(letterHash.keys()):
+            if letter in letterHash[letter]:
+                return False
+    def answer(self, letterHash, allLetters) -> str:
+        hierarchy = []
+        graphed_letters = set(letterHash.keys())
+        ordered_letters = sorted(graphed_letters, key=lambda x: len(letterHash[x]))
+        wild_cards = list(allLetters - graphed_letters)
+        hierarchy += ordered_letters + wild_cards
+        return "".join(hierarchy)
+    def foreignDictionary(self, words: List[str]) -> str:
+        lowerLetterHash = {}
+        higherLetterHash = {}
+        allLetters = {letter for word in words for letter in word}
+        self.compareLetters(words, lowerLetterHash, higherLetterHash)
+        # self.mergeSets(lowerLetterHash)
+        self.mergeSets(higherLetterHash)
+        # print (higherLetterHash)
+        # if self.validateHash(lowerLetterHash) == False:
+        #     return ""
+        if self.validateHash(higherLetterHash) == False:
+            return ""
+        return self.answer(higherLetterHash, allLetters)
