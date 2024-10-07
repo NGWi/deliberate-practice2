4b4ee022a8d3793c3d8cb86e473a481089e882f5
Nathan G. Wiseman <55075327+NGWi@users.noreply.github.com>
2024-10-07 10:34:22 -0400

    Tested tweaks and found better for 1000 num.

--- Blind 75/maxSubarray.py
+++ Blind 75/maxSubarray.py
@@ -72,6 +72,42 @@ class Solution:
     def maxSubArray5(self, nums: List[int]) -> int:
         sums = reduce(lambda sums, num: (max(sums[0], sums[1] + num), max(sums[1] + num, num)), nums[1:], (max(nums), nums[0]))
         return sums[0]
+    
+    def maxSubArray6(self, nums: List[int]) -> int:
+      max_sum = current_sum = nums[0]
+      
+      for num in nums[1:]:
+          if current_sum < num:
+              current_sum = num
+          current_sum += num
+          if current_sum > max_sum:
+              max_sum = current_sum
+      
+      return max_sum
+  
+    def maxSubArray7(self, nums: List[int]) -> int:
+      max_sum = current_sum = nums[0]
+      
+      for num in nums[1:]:
+          if current_sum < 0:
+              current_sum = num
+          else: current_sum += num
+          if current_sum > max_sum:
+              max_sum = current_sum
+              
+      return max_sum
+  
+    def maxSubArray8(self, nums: List[int]) -> int:
+      max_sum = current_sum = nums[0]
+      
+      for num in nums[1:]:
+          if current_sum + num < num:
+              current_sum = num
+          else: current_sum += num
+          if current_sum > max_sum:
+              max_sum = current_sum
+              
+      return max_sum 
       
 
     
@@ -84,6 +120,9 @@ def comparePerf():
     total_time3 = 0
     total_time4 = 0
     total_time5 = 0
+    total_time6 = 0
+    total_time7 = 0
+    total_time8 = 0
     for i in range(1000):
         nums = [random.randint(-1000, 1000) for _ in range(random.randint(1, 10000))]
         start = time.process_time()
@@ -110,12 +149,31 @@ def comparePerf():
         sol.maxSubArray5(nums)
         end = time.process_time()
         total_time5 += end - start
+        
+        start = time.process_time()
+        sol.maxSubArray6(nums)
+        end = time.process_time()
+        total_time6 += end - start
+        
+        start = time.process_time()
+        sol.maxSubArray7(nums)
+        end = time.process_time()
+        total_time7 += end - start
+        
+        start = time.process_time()
+        sol.maxSubArray8(nums)
+        end = time.process_time()
+        total_time8 += end - start
+        
     print (f"Average of maxSubArray took {total_time/1000} seconds to run.")
     print (f"Average of maxSubArray2 took {total_time2/1000} seconds to run.")
     print (f"Average of maxSubArray3 took {total_time3/1000} seconds to run.")
     print (f"Average of maxSubArray4 took {total_time4/1000} seconds to run.")
     print (f"Average of maxSubArray5 took {total_time5/1000} seconds to run.")
-        
+    print (f"Average of maxSubArray6 took {total_time6/1000} seconds to run.")
+    print (f"Average of maxSubArray7 took {total_time7/1000} seconds to run.")
+    print (f"Average of maxSubArray8 took {total_time8/1000} seconds to run.")
+            
 comparePerf()
 
 # Ave. for 1000 runs of nums.length <= 1000:
@@ -124,11 +182,18 @@ comparePerf()
 # 1.5 e-05 sec for function3
 # 4.5-5 e-05 sec for function4
 # 10-11 e-05 sec for function5
+# function6 is 1% slower than function5
+# function7 is 3-4% faster than function5
+# function8 (interviewer's solution) = 2 e-05 sec
+
 
 # Ave. for 1000 runs of nums.length <= 10000 took around 9 times as long.
-# Average of maxSubArray took 0.00017863299999999776 seconds to run.
-# Average of maxSubArray2 took 0.0007111129999999952 seconds to run.
-# Average of maxSubArray3 took 0.00013698999999999862 seconds to run.
-# Average of maxSubArray4 took 0.00042888099999999484 seconds to run.
-# Average of maxSubArray5 took 0.000991572999999997 seconds to run.
+# Average of maxSubArray took 0.00017761199999999993 seconds to run.
+# Average of maxSubArray2 took 0.0007025039999999964 seconds to run.
+# Average of maxSubArray3 took 0.00013555100000000505 seconds to run. !!!!
+# Average of maxSubArray4 took 0.0004221879999999903 seconds to run.
+# Average of maxSubArray5 took 0.0009809480000000123 seconds to run.
+# Average of maxSubArray6 took 0.00013817699999999096 seconds to run.
+# Average of maxSubArray7 took 0.0001361130000000016 seconds to run. ????
+# Average of maxSubArray8 took 0.00018253600000000563 seconds to run.
 
