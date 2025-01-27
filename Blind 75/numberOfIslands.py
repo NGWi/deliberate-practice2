c9d6f83dfc32f09631983e580bd85fb0a8daacfa
Nathan G. Wiseman <55075327+NGWi@users.noreply.github.com>
2025-01-27 07:02:08 -0500

    It was just the print statements crashing them, lol.

--- Blind 75/numberOfIslands.py
+++ Blind 75/numberOfIslands.py
@@ -1,29 +1,25 @@
 from typing import List
 class Solution:
-    """ Crashes LeetCode and NeetCode drivers.
     def __init__(self):
-            self.directions = [(-1, 0), (0, -1), (1, 0), (0, 1)]
-            self.island_n = 1
-    """
+        self.directions = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+        self.island_n = 1
         
     def prop(self, row_n, cell_n, cell):
         print(row_n, cell_n, "(", cell, ")")
         if cell == "1":
             self.new_island = True
-            self.grid[row_n][cell_n] = "0"
+            self.g[row_n][cell_n] = "0"
             for direction in self.directions:
                 x = row_n + direction[0]
                 y = cell_n + direction[1]
-                if x < self.rows and x >= 0 and y < self.cols and y >= 0:
+                if x < self.r and x >= 0 and y < self.c and y >= 0:
                     print(row_n, cell_n, "(", cell, ")", "=>", x, y)
-                    self.prop(x, y, self.grid[x][y])
+                    self.prop(x, y, self.g[x][y])
         
-    def numIslands(self, grid: List[List[str]]) -> int:
-        self.grid = grid
-        self.rows = len(grid)
-        self.cols = len(grid[0])
-        self.directions = [(-1, 0), (0, -1), (1, 0), (0, 1)]
-        self.island_n = 1
+    def numIslands_propagate(self, grid: List[List[str]]) -> int:
+        self.g = grid
+        self.r = len(grid)
+        self.c = len(grid[0])
         self.new_island = False
         for row_n, row in enumerate(grid):
             for cell_n, cell in enumerate(row):
@@ -34,6 +30,68 @@ class Solution:
                     self.new_island = False
         return self.island_n - 1
 
+    def stackify(self, row_n, cell_n, cell):
+        print(row_n, cell_n, "(", cell, ")")
+        if cell == "1":
+            self.g[row_n][cell_n] = "0"
+            for direction in self.directions:
+                x = row_n + direction[0]
+                y = cell_n + direction[1]
+                if x < self.r and x >= 0 and y < self.c and y >= 0:
+                    print(row_n, cell_n, "(", cell, ")", "=>", x, y)
+                    self.stack.append((x, y))
+            while self.stack:
+                x, y = self.stack.pop()
+                self.stackify(x, y, self.g[x][y])
+
+    def numIslands_stackify(self, grid: List[List[str]]) -> int: # Stackify
+        if not grid:
+            return 0
+        self.g = grid
+        self.r = len(grid)
+        self.c = len(grid[0])
+        self.new_island = False
+        for row_n, row in enumerate(grid):
+            for cell_n, cell in enumerate(row):
+                print("Next cell")
+                if cell == "1":
+                    self.new_island = True
+                    self.stack = []
+                    self.stackify(row_n, cell_n, cell)
+                if self.new_island:
+                    self.island_n += 1
+                    self.new_island = False
+
+        return self.island_n - 1
+
+    def dfs(self, row_n, cell_n, cell):
+        print(row_n, cell_n, "(", cell, ")")
+        if cell == "1":
+            self.g[row_n][cell_n] = "0"
+            for direction in self.directions:
+                x = row_n + direction[0]
+                y = cell_n + direction[1]
+                if x < self.r and x >= 0 and y < self.c and y >= 0:
+                    print(row_n, cell_n, "(", cell, ")", "=>", x, y)
+                    self.dfs(x, y, self.g[x][y])
+        return
+
+    def numIslands(self, grid: List[List[str]]) -> int: # DFS-Style coding
+        if not grid:
+            return 0
+        self.g = grid
+        self.r = len(grid)
+        self.c = len(grid[0])
+        self.island_n = 0
+        for row_n, row in enumerate(grid):
+            for cell_n, cell in enumerate(row):
+                print("Next cell")
+                if cell == "1":
+                    self.island_n += 1
+                    self.dfs(row_n, cell_n, cell)
+
+        return self.island_n
+        
 # Test case 1:
 grid = [
     ["0","1","1","1","0"],
